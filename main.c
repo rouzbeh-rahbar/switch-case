a2a30fbcbfc49b2c68acaa6d9ceb0dceaa012b9f
rouzbeh-rahbar <rouzbeh.rahbar4@gmail.com>
2020-12-09 20:49:20 +0100

    first commit

--- main.c
+++ main.c
@@ -2,28 +2,14 @@
 
 int main(void) 
 {
-  int a, z1, z2;
-  printf("case-Beispiel\n\n");
-  printf("Bitte wählen Sie eine Aktion:\n");
-  printf("1 ... Addition\n");
-  printf("2 ... Subtraktion\n");
-  printf("3 ... Multiplikation\n");
-  printf("Ihre Auswahl: ");
-  scanf("%d", &a);  // die getroffene Auswahl
-  printf("Bitte 1. Zahl eingeben: ");
-  scanf("%d",&z1);
-  printf("Bitte 2. Zahl eingeben: ");
-  scanf("%d",&z2);
-  switch(a)
+  char ch; // Character-Variable = ein Zeichen
+  printf("Dieses Programm gibt 'Hello World' aus und fragt danach, ob es nochmal ausgeführt werden soll.\n\n");
+  do
   {
-    case 1: printf("%d + %d = %d\n", z1, z2, z1+z2); break;
-    case 2: printf("%d - %d = %d\n", z1, z2, z1-z2); break;
-    case 3: printf("%d * %d = %d\n", z1, z2, z1*z2); break;
-    default: printf("Ihre Auswahl war ungültig\n\n");
-  }
-  
-  printf("Programmende.\n\n");
-  
+    printf("Hello World\n");
+    printf("Soll 'Hello World' nochmal ausgegeben werden (j/n)? ");
+    scanf(" %c", &ch); // Leerzeichen wichtig!
+  } while(ch!='n' && ch!='N');
 
   return 0;
 }
